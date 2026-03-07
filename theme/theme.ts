f148ef7135e19cd2614057fa0f37cb56dd490ec5
FaizuRrehman <m.faizurrehman.business@gmail.com>
2026-03-07 21:20:31 +0500

    feat: initial commit

---
+++ theme/theme.ts
@@ -0,0 +1,35 @@
+export const theme = {
+    colors: {
+      background: "#F7F7F8",
+      surface: "#FFFFFF",
+  
+      primary: "#48E44B",
+      primaryAccent: "#9EFF3E",
+  
+      text: {
+        primary: "#141519",
+        secondary: "#767F88",
+        muted: "#E5E5E7",
+      },
+  
+      dark: "#1A1B1E",
+      border: "#E5E5E7",
+    },
+  
+    font: {
+      primary: "Anderson Grotesk, sans-serif",
+    },
+  
+    radius: {
+      sm: "6px",
+      md: "10px",
+      lg: "16px",
+    },
+  
+    container: {
+      maxWidth: "1280px",
+      padding: "24px",
+    },
+  
+    transition: "all 0.25s ease",
+  };
